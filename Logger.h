7fefeff0553df6afd1b72ac44d6fc7e0801eaf39
korneliuszw <korneliuszwstudio@gmail.com>
2024-04-18 01:19:13 +0200

    remove debug things from logger

--- Logger.h
+++ Logger.h
@@ -20,7 +20,6 @@ class LoggerSystem {
         DebugLogger& debugLogger;
         std::stringstream lastLogStream;
         bool flushNext = false;
-        int state = 0;
 
     public:
         int n = 0;
@@ -44,12 +43,10 @@ template<typename T>
 LoggerSystem::InfoLogger &LoggerSystem::InfoLogger::operator<<(const T &value) {
     // we want to keep only last line of logs at a time so clear it after we flushed other outputs
     if (flushNext) {
-        this->infoLogFile << " from str logger flush: " <<  lastLogStream.str() << std::endl;
         lastLogStream.str("");
         lastLogStream.clear();
         flushNext = false;
     }
-    state += 1;
     lastLogStream << value;
     this->debugLogger.debugLogFile << value;
     this->infoLogFile << value;
